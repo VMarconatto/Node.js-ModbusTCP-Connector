a6e17377cd14a60b926c6c9bf6f17489943667ff
VMarconatto <maintechautomacao@gmail.com>
2024-04-06 02:31:31 -0300

    tste
    s

---
+++ mongo.js
@@ -0,0 +1,105 @@
+require('dotenv').config()
+const { MongoClient } = require('mongodb')
+const uri = process.env.connectionstring
+const client = new MongoClient(uri)
+const DatasReception = require('./src/controllers/servermodbus.js')
+
+const hosts = `192.168.1.13`
+const ping = require('ping')
+
+client.connect().then(console.log(`Mongo Client Connected`)).catch((e) => console.log(e))
+const aggDB = client.db("MilkMaster").collection("MilkReceptions")
+
+class MilkReception {
+    constructor() {
+    }
+    crudMDB() {
+
+        aggDB.insertMany([{
+            "metadata": {
+                "Recep_Id": 1, "type": "Equipament", "ProdUnit": "M³/Dia", "Job": "Recepção 1"
+            },
+            "timestamp": new Date(), "LT20_PV": DatasReception.holding.readFloatBE(0), "FT20_PV": DatasReception.holding.readFloatBE(4),
+            "PT20_PV": DatasReception.holding.readFloatBE(8), "TT20_PV": DatasReception.holding.readFloatBE(12), "TT20_PV": DatasReception.holding.readFloatBE(16),
+            "TT20_PV": DatasReception.holding.readFloatBE(20), "ModbusTCPFail": false
+        },
+        {
+            "metadata": {
+                "Recep_Id": 2, "type": "Equipament", "ProdUnit": "M³/Dia", "Job": "Recepção 2"
+            },
+            "timestamp": new Date(), "LT01_PV": DatasReception.holding.readFloatBE(24), "FT01_PV": DatasReception.holding.readFloatBE(28),
+            "PT01_PV": DatasReception.holding.readFloatBE(32), "TT01_PV": DatasReception.holding.readFloatBE(36), "TT02_PV": DatasReception.holding.readFloatBE(40),
+            "TT03_PV": DatasReception.holding.readFloatBE(44), "ModbusTCPFail": false
+        },
+        {
+            "metadata": {
+                "Recep_Id": 3, "type": "Equipament", "ProdUnit": "M³/Dia", "Job": "Recepção 3"
+            },
+            "timestamp": new Date(), "LT01_PV": DatasReception.holding.readFloatBE(48), "FT01_PV": DatasReception.holding.readFloatBE(52),
+            "PT01_PV": DatasReception.holding.readFloatBE(56), "TT01_PV": DatasReception.holding.readFloatBE(60), "TT02_PV": DatasReception.holding.readFloatBE(64),
+            "TT03_PV": DatasReception.holding.readFloatBE(68), "ModbusTCPFail": false
+        },
+        {
+            "metadata": {
+                "Recep_Id": 4, "type": "Equipament", "ProdUnit": "M³/Dia", "Job": "Recepção 4"
+            },
+            "timestamp": new Date(), "LT01_PV": DatasReception.holding.readFloatBE(72), "FT01_PV": DatasReception.holding.readFloatBE(76),
+            "PT01_PV": DatasReception.holding.readFloatBE(80), "TT01_PV": DatasReception.holding.readFloatBE(84), "TT02_PV": DatasReception.holding.readFloatBE(88),
+            "TT03_PV": DatasReception.holding.readFloatBE(92), "ModbusTCPFail": false
+
+        },]).then((mbdata) => console.log(mbdata)).catch((e) => console.log(e))
+    }
+
+    mbdatafail() {
+
+        aggDB.insertMany([{
+            "metadata": {
+                "Recep_Id": 1, "type": "Equipament", "ProdUnit": "M³/Dia", "Job": "Recepção 1"
+            },
+            "timestamp": new Date(), "LT20_PV": null, "FT20_PV": null,
+            "PT20_PV": null, "TT20_PV": null, "TT20_PV": null,
+            "TT20_PV": null, "ModbusTCPFail": true
+        },
+        {
+            "metadata": {
+                "Recep_Id": 2, "type": "Equipament", "ProdUnit": "M³/Dia", "Job": "Recepção 2"
+            },
+            "timestamp": new Date(), "LT01_PV": null, "FT01_PV": null,
+            "PT01_PV": null, "TT01_PV": null, "TT02_PV": null,
+            "TT03_PV": null, "ModbusTCPFail": true
+        },
+        {
+            "metadata": {
+                "Recep_Id": 3, "type": "Equipament", "ProdUnit": "M³/Dia", "Job": "Recepção 3"
+            },
+            "timestamp": new Date(), "LT01_PV": null, "FT01_PV": null,
+            "PT01_PV": null, "TT01_PV": null, "TT02_PV": null,
+            "TT03_PV": null, "ModbusTCPFail": true
+        },
+        {
+            "metadata": {
+                "Recep_Id": 4, "type": "Equipament", "ProdUnit": "M³/Dia", "Job": "Recepção 4"
+            },
+            "timestamp": new Date(), "LT01_PV": null, "FT01_PV": null,
+            "PT01_PV": null, "TT01_PV": null, "TT02_PV": null,
+            "TT03_PV": null, "ModbusTCPFail": true
+        },
+        ]).then((mbdata)=>console.log(mbdata)).catch((e)=>{console.log(e)})
+    }
+
+}
+
+const MillReceptionMBTCP = new MilkReception()
+
+async function start() {
+
+    setInterval(() => {
+        ping.sys.probe(hosts, (isAlive) => {
+            var msg = isAlive ? console.log(`Host MB Partner: ${hosts} is alive`) + MillReceptionMBTCP.crudMDB():
+            console.log(`Host MB Partner: ${hosts} is not alive`) + MillReceptionMBTCP.mbdatafail()
+        })
+    }, 2000)
+
+}
+
+start()
---
+++ src/controllers/servermodbus.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+
+const modbus = require('jsmodbus')
+const net = require('net')
+const netServer = new net.Server()
+const holding = Buffer.alloc(10000)
+const server = new modbus.server.TCP(netServer, {
+    holding: holding
+})
+
+const { error } = require('console')
+const hosts = `192.168.1.13`
+const { disconnect } = require('process')
+
+
+class ModbusTCPRun {
+    constructor(isAlive = false) {
+        this.connection = null
+        this.isConnected = false
+        this.reconnectDelay = 4000
+
+    }
+
+    connect(value = []) {
+        this.connection = server
+        this.connection.on(error, (err) => {
+            console.error(`Modbus Connection Error: ${err}`)
+            this.handleDisconnect()
+        })
+
+        console.log('entrou no connect')
+
+        this.isConnected = true
+        return value = this.sendRequest()
+    }
+
+    handleDisconnect() {
+        console.log('entrou no erro')
+        this.isConnected = false
+        setTimeout(() => {
+            this.connect()
+        }, this.reconnectDelay);
+    }
+
+    sendRequest() {
+
+        const mbvalues = server.on('postWriteMultipleRegisters', () => {
+            const register = server.holding.buffer.slice(0, 12)
+            const buff = Buffer.from(register)
+            const json = buff.toJSON()
+            return [...json.data]
+        })
+        return mbvalues
+    }
+}
+
+const mbData = new ModbusTCPRun()
+mbData.connect()
+
+const DatasReception = mbData.sendRequest()
+
+
+netServer.listen(502)
+
+module.exports = DatasReception
+
+
